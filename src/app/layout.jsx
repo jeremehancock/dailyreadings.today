7e6d661592c96199bdc6080d047d7e4a832094c4
Jereme <michael@jeremehancock.com>
2024-10-30 14:04:06 -0500

    fixed favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 // Define the metadata for the layout
 export const metadata = {
-  title: 'DailyReadings.today',
+  title: 'Daily Mass Readings',
   description: 'Daily Mass Readings',
   generator: 'Next.js',
   manifest: '/manifest.json',
@@ -17,7 +17,7 @@ export const metadata = {
   ],
   icons: [
     { rel: 'apple-touch-icon', url: '/icons/apple-touch-icon.png' },
-    { rel: 'icon', url: '/icons/favicon-96x96.png' },
+    { rel: 'icon', url: '/icons/favicon.svg' },
     { rel: 'shortcut-icon', url: '/icons/favicon.ico' },
   ],
 };
