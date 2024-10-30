050dd82d2827652c9478e78b9b2476c03ab714ac
Jereme <michael@jeremehancock.com>
2024-10-30 13:15:45 -0500

    PWA

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
-// layout.jsx
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -29,6 +29,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="description" content="Daily Mass Readings" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/icons/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/icons/site.webmanifest" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
