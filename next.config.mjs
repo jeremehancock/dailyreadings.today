12d35873d021c530b5dd837bf03d2ac0a37859b7
Jereme <michael@jeremehancock.com>
2024-10-30 12:29:29 -0500

    PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+  dest: 'public', // where to output the service worker files
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // disable PWA in development
+  // You can add other PWA-specific options here
+});
+
+// Export the configuration with other Next.js settings if needed
+export default {
+  ...nextConfig,
+  // Add any other Next.js configurations here
+};
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,10 +3,16 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/theme';
+import Head from 'next/head';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Daily Mass Readings" />
+      </Head>
       <body>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
